6eb8b910c50c6bc174e2c2636f521bc1befb0bd6
saisampath33 <sampathmay10@gmail.com>
2025-07-23 09:26:06 +0530

    updated frp PWA app on mobile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -86,7 +86,7 @@ export default function Profile() {
         />
       </div>
       <button type="button" className="update-btn" onClick={handleSubmit}>
-         Update Profile
+        Update Profile
       </button>
     </div>
   );
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { vitePWA } from 'vite-plugin-pwa';
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "Cup of Coffee",
+        short_name: "CoC",
+        description: "MERN Stack",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/icons/sun-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/sun-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/icons/sun-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),],
 })
